3ca12dc6d2175ee66b85222f57078bb43dbd7240
Shashidhar Ravishankar <shashidhar.r.shankar@gmail.com>
2025-02-06 12:25:40 -0800

    Parser package __init__

---
+++ parsers/__init__.py
@@ -0,0 +1,4 @@
+from .utils import parse_imgt_four_digit, transform_mhc_restriction, get_mhc_sequence, process_mhc_restriction
+
+# Optionally, specify the public interface
+__all__ = ["parse_imgt_four_digit", "transform_mhc_restriction", "get_mhc_sequence", "process_mhc_restriction"]
