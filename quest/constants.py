b8ce2d810a168686e88635bf3b88d1a0c2f18ec5
Shashidhar Ravishankar <shashidhar.r.shankar@gmail.com>
2025-03-25 12:25:45 -0700

    Modified model config and constants

---
+++ quest/constants.py
@@ -0,0 +1,21 @@
+PAD_TOKEN_ID = 0
+SPECIAL_TOKENS = [
+        "[PAD]", "[UNK]", "[ETRA]", "[ETRB]", "[EPEP]", "[EMHO]", "[EMHT]", 
+        "[TRA]", "[TRB]", "[PEP]", "[MHO]", "[MHT]", "[END]"
+    ]
+TRA_TOKENS = ["[TRA]", "[ETRA]"]
+TRB_TOKENS = ["[TRB]", "[ETRB]"]
+PEP_TOKENS = ["[PEP]", "[EPEP]"]
+MHC1_TOKENS = ["[MHO]", "[EMHO]"]
+MHC2_TOKENS = ["[MHT]", "[EMHT]"]
+PAD_TOKEN_STR = "[PAD]"
+UNK_TOKEN_STR = "[UNK]"
+TRA_START = "[TRA]"
+TRB_START = "[TRB]"
+PEP_START = "[PEP]"
+MHC1_START= "[MHO]"
+MHC2_START= "[MHT]"
+END_TOKEN = "[END]"
+TRA_LENGTH = 8
+TRB_LENGTH = 8
+PEPTIDE_LENGTH = 4
